ff39f7764714f16a27adac6d9cb4cc228e822cc9
banzayche <dharm.serg@gmail.com>
2020-10-05 13:10:32 +0300

    Adding styles for plot, axis, etc..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,16 @@ export class AppComponent implements OnInit {
   barChartPopulation() {
     HighCharts.chart('columnChart', {
       chart: {
-        zoomType: 'xy'
+        zoomType: 'xy',
+        plotBackgroundColor: '#fff',
+        backgroundColor: 'transparent',
+        showAxes: true
       },
       title: {
         text: 'CHART TITLE',
         style: {
-          fontFamily: 'Arial'
+          fontFamily: 'Arial',
+          color: 'rgb(72,72,72)'
         },
         align: 'left',
         margin: 20,
@@ -34,15 +38,20 @@ export class AppComponent implements OnInit {
         // categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
         //   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         // crosshair: true,
+        title: {
+          style: {
+            color: 'rgb(128, 128, 128)'
+          }
+        },
         units: [
           [
-          'minute',
-          [15, 30]
-        ],
+            'minute',
+            [15, 30]
+          ],
           [
-          'hour',
-          [1]
-        ]],
+            'hour',
+            [1]
+          ]],
         uniqueNames: true,
         type: 'datetime',
         // dateTimeLabelFormats: {
@@ -54,14 +63,21 @@ export class AppComponent implements OnInit {
             // TODO define H:M or H AM/PM
             return this.value;
           }
-        }
+        },
+        reversed: false,
+        gridLineWidth: 1,
+        gridLineColor: 'rgb(250,250,250)',
+        minorTickLength: 0,
+        tickLength: 0,
+        lineColor: 'rgb(214,214,214)',
+        lineWidth: 1
       }],
       yAxis: [{ // Primary yAxis
         labels: {
           // format: '{value}°C',
-          // style: {
-          //   color: Highcharts.getOptions().colors[1]
-          // }
+          style: {
+            // color: Highcharts.getOptions().colors[1]
+          }
         },
         title: {
           text: 'Instance Count',
@@ -72,16 +88,20 @@ export class AppComponent implements OnInit {
           rotation: 0,
           y: -5,
           x: 20,
-          offset: -50
+          offset: -50,
           // style: {
           //   color: Highcharts.getOptions().colors[1]
           // }
-        }
+        },
+        lineColor: 'rgb(214,214,214)',
+        lineWidth: 1,
       }, { // Secondary yAxis
+        gridLineColor: 'rgb(250,250,250)',
         title: {
           text: 'milicores',
           style: {
-            fontFamily: 'Arial'
+            fontFamily: 'Arial',
+            color: 'rgb(128, 128, 128)'
           },
           align: 'high',
           rotation: 0,
@@ -95,7 +115,8 @@ export class AppComponent implements OnInit {
         labels: {
           // format: '{value} mm',
           // style: {
-          //   color: Highcharts.getOptions().colors[0]
+            // color: Highcharts.getOptions().colors[0]
+            // color: 'rgb(128, 128, 128)'
           // }
         },
         opposite: true
@@ -110,8 +131,8 @@ export class AppComponent implements OnInit {
         verticalAlign: 'top',
         y: -10,
         floating: true,
-        backgroundColor:
-          'rgba(255,255,255,0.25)'
+        // backgroundColor:
+        //   'rgba(255,255,255,0.25)'
       },
       series: [{
         name: 'Rainfall',
