a94eac7828babff6e1824793752380786338ea99
banzayche <dharm.serg@gmail.com>
2020-10-07 17:50:42 +0300

    Handling click events. Updating styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import * as HighCharts from 'highcharts';
 import * as moment from 'moment';
 import {ApiServiceService} from './api-service/api-service.service';
 import {DataAdaptationService} from './utilities/data-adaptation.service';
-import {map} from 'rxjs/operators';
+import {map, takeUntil} from 'rxjs/operators';
 import {GraphDataItem} from './utilities/models';
+import {fromEvent} from 'rxjs';
+import {BaseUnsubscribe} from './utilities/base-unsubscribe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'Ola Amigo';
-  chart;
-
-  constructor(private service: ApiServiceService, private dataAdaptation: DataAdaptationService) {
+export class AppComponent extends BaseUnsubscribe implements OnInit {
+  constructor(
+    private service: ApiServiceService,
+    private dataAdaptation: DataAdaptationService,
+    private elementRef: ElementRef) {
+    super();
   }
 
   ngOnInit() {
     this.service.getChartsData().pipe(
       map(data => this.dataAdaptation.getPreparedData(data))
     ).subscribe(data => {
-
       this.barChartPopulation(data);
     });
   }
 
   barChartPopulation(data: GraphDataItem[][]): void {
-    function doThis(e) {
-      console.log(e);
-    }
-
-    this.chart = HighCharts.chart('demo-chart', {
+    HighCharts.chart('demo-chart', {
       chart: {
         zoomType: 'xy',
         plotBackgroundColor: '#fff',
         backgroundColor: 'transparent',
         showAxes: true,
+        events: {
+          load: this.onLoadChart.bind(this),
+          redraw: chart => this.updateLegendCheckboxesState(chart.target)
+        }
+      },
+      lang: {
+        noData: 'Provide some data',
       },
       title: {
-        text: 'CHART TITLE',
+        useHTML: true,
+        text: 'CHART TITLE' + ' ' + '<button class="refresh-data" data-refresh>REFRESH DATA</button>',
         style: {
           fontFamily: 'Arial',
           color: 'rgb(72,72,72)'
@@ -60,9 +66,8 @@ export class AppComponent implements OnInit {
           },
           events: {
             legendItemClick(e) {
-              console.log(e, e.target);
               return false;
-            }  // disable legend click
+            }
           }
         },
         column: {
@@ -95,7 +100,6 @@ export class AppComponent implements OnInit {
             return value.toUpperCase();
           },
           style: {
-            // TODO color: Highcharts.getOptions().colors[0]
             color: 'rgb(0,0,0)'
           }
         },
@@ -106,9 +110,6 @@ export class AppComponent implements OnInit {
         tickLength: 0,
         lineColor: 'rgb(214,214,214)',
         lineWidth: 1,
-        // TODO not sure about this
-        // min: data[0][0].x > data[1][0].x ? data[1][0].x : data[0][0].x,
-        // max: data[0][data[0].length - 1].x > data[1][data[1].length - 1].x ? data[0][data[0].length - 1].x : data[1][data[1].length - 1].x
       }],
       yAxis: [{ // Primary yAxis
         labels: {
@@ -168,25 +169,19 @@ export class AppComponent implements OnInit {
         symbolHeight: 0,
         squareSymbol: false,
         labelFormatter() {
-          return `<input type="checkbox" (change)="doThis($event)" data-target-checkbox data-series="${this.name}" checked> ${this.name}`;
+          return `<input class="legend-checkbox" type="checkbox" data-legend-checkbox data-series="${this.name}"> ${this.name}`;
         }
       },
       series: [{
         name: 'Rainfall',
         type: 'column',
         yAxis: 1,
-        // events: {
-        //   legendItemClick: () => false  // disable legend click
-        // },
         color: 'rgb(126,203,235)',
         data: data[1]
       },
         {
           name: 'Temperature',
           type: 'spline',
-          // events: {
-          //   legendItemClick: () => false  // disable legend click
-          // },
           color: 'rgb(0,83,157)',
           data: data[0]
         }
@@ -194,18 +189,43 @@ export class AppComponent implements OnInit {
     } as any);
   }
 
-  refresh() {
+  private onLoadChart(e) {
+    const chart = e.target;
+    const chartDom = this.elementRef.nativeElement.querySelector('#demo-chart');
+    this.updateLegendCheckboxesState(chart);
+    fromEvent(chartDom, 'click').pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe((clickEv: any) => {
+      if (clickEv.target.hasAttribute('data-legend-checkbox')) {
+        this.toggleSeries(chart, clickEv);
+      } else if (clickEv.target.hasAttribute('data-refresh')) {
+        this.refresh(chart);
+      }
+    });
+  }
+
+  updateLegendCheckboxesState(chart) {
+    this.elementRef.nativeElement.querySelector('[data-series="Rainfall"]').checked = chart.series[0].visible;
+    this.elementRef.nativeElement.querySelector('[data-series="Temperature"]').checked = chart.series[1].visible;
+  }
+
+  toggleSeries(chart, clickEv) {
+    const seriesIndex = clickEv.target.getAttribute('data-series') === 'Rainfall' ? 0 : 1;
+    if (chart.series[seriesIndex].visible) {
+      chart.series[seriesIndex].hide();
+    } else {
+      chart.series[seriesIndex].show();
+    }
+  }
+
+  refresh(chart) {
     this.service.getChartsData(true).pipe(
       map(data => this.dataAdaptation.getPreparedData(data))
     ).subscribe(data => {
 
-      this.chart.series.forEach((seriesItem, index) => {
+      chart.series.forEach((seriesItem, index) => {
         seriesItem.setData(data[index], true);
       });
-
-      // this.chart.redraw();
-
-      // this.chart.series[0].setData([{x:1,y:1}], true, true);
     });
   }
 }
