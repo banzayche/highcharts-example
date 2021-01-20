657ecab8d49b92dfbdbd683a1a54f73a42841328
banzayche <dharm.serg@gmail.com>
2021-01-20 11:49:38 +0200

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,9 +10,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-coverage'),
-      require('karma-mocha-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -23,10 +22,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['mocha','coverage'],
-    coverageReporter: {
-      type: 'text-summary'
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
